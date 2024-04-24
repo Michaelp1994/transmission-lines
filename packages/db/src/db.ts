e7f59c633ca492393526d6cf87f1247fe3f213ed
Michael Poulgrain <michael.poulgrain@gmail.com>
2024-04-24 17:03:45 -0300

    I can now use native modules in both electron and node at the same time, by prebuilding and pointing to .node file

--- packages/db/src/db.ts
+++ packages/db/src/db.ts
@@ -1,24 +1,20 @@
+import Connection, { Database } from "better-sqlite3";
 import {
     type BetterSQLite3Database,
     drizzle,
 } from "drizzle-orm/better-sqlite3";
-import nodeGyp from "node-gyp-build";
 
 import * as schema from "./schemas";
 
-// const Connection = require("@repo/better-sqlite3");
-
-const Connection = nodeGyp("../../node_modules/better-sqlite3");
-
 export interface DBContext {
-    conn: any;
+    conn: Database;
     db: BetterSQLite3Database<typeof schema>;
 }
 
 function databaseInit(path: string): DBContext {
-    // console.log(Connection);
-    const conn = new Connection.Database(path, "", true);
-    console.log(conn);
+    const conn = new Connection(path, {
+        nativeBinding: "../../prebuilds/win32-x64/better-sqlite3.node",
+    });
     const db = drizzle(conn, { schema });
 
     console.log("dataSource initialized");
