12a0e8f061c55e28c57ffc56e2a26153e8d8a005
Michael Poulgrain <michael.poulgrain@gmail.com>
2024-10-18 09:45:21 +1000

    version bump

--- packages/api/src/routers/meta.ts
+++ packages/api/src/routers/meta.ts
@@ -3,7 +3,7 @@ import { publicProcedure, router } from "../trpc";
 export default router({
     hello: publicProcedure.query(() => "hello world!"),
     version: publicProcedure.query(() => {
-        const version = process.env["npm_package_version"] || "1.0.2";
+        const version = process.env["npm_package_version"] || "1.0.3";
         return version;
     }),
 });
