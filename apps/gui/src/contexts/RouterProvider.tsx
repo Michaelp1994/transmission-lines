c2c636db79c7fa770667176ffc2d5163be51bc5e
Michael Poulgrain <michael.poulgrain@gmail.com>
2024-10-17 10:41:10 +1000

    bug: packaged app can't use browser history, changed to hash

--- apps/gui/src/contexts/RouterProvider.tsx
+++ apps/gui/src/contexts/RouterProvider.tsx
@@ -1,12 +1,14 @@
 import {
+    createHashHistory,
     createRouter,
     RouterProvider as ReactRouterProvider,
 } from "@tanstack/react-router";
 
 import { routeTree } from "~/routeTree.gen";
+const hashHistory = createHashHistory();
 
 // Create a new router instance
-const router = createRouter({ routeTree });
+const router = createRouter({ routeTree, history: hashHistory });
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
