1142bb36fe4cc6c1dc17c54140ce61a9b971b051
Michael Poulgrain <michael.poulgrain@gmail.com>
2024-10-22 21:14:21 +1000

    fix: format

--- packages/ui/src/components/data-table/DataTableRowCheckbox.tsx
+++ packages/ui/src/components/data-table/DataTableRowCheckbox.tsx
@@ -3,17 +3,17 @@ import { type Row } from "@tanstack/react-table";
 import { Checkbox } from "../checkbox";
 
 interface DataTableRowCheckboxProps<T> {
-  row: Row<T>;
+    row: Row<T>;
 }
 
 export function DataTableRowCheckbox<T>({ row }: DataTableRowCheckboxProps<T>) {
-  return (
-    <Checkbox
-      checked={row.getIsSelected()}
-      disabled={!row.getCanSelect()}
-      onCheckedChange={() => {
-        row.toggleSelected();
-      }}
-    />
-  );
+    return (
+        <Checkbox
+            checked={row.getIsSelected()}
+            disabled={!row.getCanSelect()}
+            onCheckedChange={() => {
+                row.toggleSelected();
+            }}
+        />
+    );
 }
