2655f0bd2cc5eb5fa73b55773169ec21015f0739
Michael Poulgrain <michael.poulgrain@gmail.com>
2024-10-23 11:24:36 +1000

    refactor(gui): deleted barrel files

--- apps/gui/src/features/conductorLocations/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as ConductorLocationTable } from "./components/ConductorLocationTable";
-export { default as CreateConductorLocationModal } from "./components/CreateConductorLocationModal";
-export { default as DeleteConductorLocationModal } from "./components/DeleteConductorLocationModal";
-export { default as UpdateConductorLocationModal } from "./components/UpdateConductorLocationModal";
